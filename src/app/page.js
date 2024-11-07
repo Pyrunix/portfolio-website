adf73d08dc65e4886f5d7ce9a33e97b52bf89530
Darren Trieu <darrentrieu03@gmail.com>
2024-11-07 12:26:06 -0500

    removed email section

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-        <EmailSection />
       </div>
       <Footer />
     </main>
